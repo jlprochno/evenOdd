82a9fe2012a420d85bd4a48378b831728bd59025
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 09:57:28 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace evenOdd;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int oddNumber = 0;
+        for (int i = 0; i < 10; ++i)
+        {
+            Console.WriteLine("Digite um número:");
+            int numberEntered = Convert.ToInt32(Console.ReadLine());
+
+            if ((numberEntered & 1) > 0)
+            ++oddNumber;
+        }
+
+        Console.WriteLine($"Você inseriu:\n{10 - oddNumber} números pares e {oddNumber} números ímpares!");
+    }
+}
